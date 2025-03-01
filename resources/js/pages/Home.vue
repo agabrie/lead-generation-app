ca2efdac6fc737839253b79027b89fe2029f58bf
agabrie <abduraghmaangabriels2@gmail.com>
2025-03-01 21:25:57 +0200

    added UserDetailsForm to Home page

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -3,9 +3,11 @@
       <h2>HOME</h2>
       <router-link class="underline" to="/test"> Take me to Test page </router-link>
       <FormTooltip>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam voluptate aut voluptatem pariatur ipsum numquam aliquid. Non praesentium numquam in?</FormTooltip>
+      <UserDetailsForm />
   </div>
 </template>
 
 <script setup>
   import FormTooltip from '@/components/FormTooltip.vue'
+  import UserDetailsForm from '@/components/forms/UserDetailsForm.vue'
 </script>
