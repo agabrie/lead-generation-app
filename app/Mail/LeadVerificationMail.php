910dff27704d0d4f32e2fe926bf68374e436c707
agabrie <abduraghmaangabriels2@gmail.com>
2025-03-01 21:28:24 +0200

    added lead verification mail

---
+++ app/Mail/LeadVerificationMail.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class LeadVerificationMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     */
+    public $mailData;
+    public function __construct($mailData)
+    {
+        //
+        $this->mailData = $mailData;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            config('mail.from.address'),
+            [],
+            [],
+            [],
+            [],
+            'Verification Mail'
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content('emails.verify-lead');
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
