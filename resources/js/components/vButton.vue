4b86048cb48528e31573bc1c158d042f00bf9e94
agabrie <abduraghmaangabriels2@gmail.com>
2025-03-02 10:45:46 +0200

    standardized theme

--- resources/js/components/vButton.vue
+++ resources/js/components/vButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <button type="submit" class="focus:ring-4 font-medium text-sm px-5 py-2.5 focus:outline-none cursor-pointer" :class="[roundedClass, buttonColors, buttonTextColors]">
+  <button type="submit" class="focus:ring-4 font-medium text-sm px-5 py-2.5 focus:outline-none" :class="[roundedClass, buttonColors, buttonTextColors, disabled?'cursor-not-allowed':'cursor-pointer']" :disabled="disabled">
     <slot />
   </button>
 </template>
@@ -13,22 +13,27 @@
     },
     'color':{
       type:String,
-      default:"blue"
+      default:"primary"
     },
     'textColor':{
       type:String,
       default:"white"
     },
+    disabled:{
+      type:Boolean,
+      default:false,
+    }
   });
 
 
   const bgColorVariants = {
-    purple: "bg-purple-700 hover:bg-purple-800 focus:ring-purple-300", 
-    blue: "bg-blue-300 hover:bg-blue-400 focus:ring-blue-300", 
-    red: "bg-red-700 hover:bg-red-800 focus:ring-red-300",
-    orange: "bg-orange-700 hover:bg-orange-800 focus:ring-orange-300",
-    yellow: "bg-yellow-700 hover:bg-yellow-800 focus:ring-yellow-300",
-    green: "bg-green-400 hover:bg-green-500 focus:ring-green-300",
+    primary: "bg-primary hover:bg-primary-subtle focus:ring-primary", 
+    secondary: "bg-secondary hover:bg-secondary-subtle focus:ring-secondary",
+    success: "bg-secosuccessndary hover:bg-success-subtle focus:ring-success",
+    error: "bg-error hover:bg-error-subtle focus:ring-error",
+    warning: "bg-warning hover:bg-warning-subtle focus:ring-warning",
+    grey: "bg-grey hover:bg-grey-subtle focus:ring-warning",
+    disabled: "bg-disabled",
   };
 
   const textColorVariants = {
@@ -41,6 +46,6 @@
     "rounded-full": props.rounded && props.rounded === "full",
   }))
 
-  const buttonColors = computed(() => bgColorVariants[props.color])
-  const buttonTextColors = computed(() => textColorVariants[props.textColor])
+  const buttonColors = computed(() => props.disabled?bgColorVariants['disabled']:bgColorVariants[props.color])
+  const buttonTextColors = computed(() => props.disabled?textColorVariants['white']:textColorVariants[props.textColor])
 </script>
